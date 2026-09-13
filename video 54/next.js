27cacc1b1c8cb5daa49baa511bc1c57f2fd81186
ziadev <ziadevdeveloper@gmail.com>
2026-09-13 12:28:02 +0500

    add file

---
+++ video 54/next.js
@@ -0,0 +1,27 @@
+
+alert("hello world")
+console.log("hello world")
+console.log("hello zia")
+console.log("hello running")
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
+
+var  a = prompt("enter your name")
+var b = prompt("enter your age")    
+var c = prompt("enter your city")
+var d = prompt("enter your country")
+
+var h = confirm("are you sure you want to submit this form")
+
+if(h ){
+    console.log("form submitted")
+}
+else{
+    console.log("form not submitted")
+}
+
+console.log("hello " + a)
+console.log("your age is " + b)
+console.log("you live in " + c)
+console.log("you are from " + d)
