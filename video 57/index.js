46449421b828803d30fb24eb0c255f580bf7f4ec
ziadev <ziadevdeveloper@gmail.com>
2026-09-16 21:02:10 +0500

    add file

---
+++ video 57/index.js
@@ -0,0 +1,30 @@
+console.log("this is tutorial for loops in javascript");
+
+// let a = 10;
+// for (let i = 0; i < 100; i++) {
+//     console.log( i);
+// }
+// let = object = {
+//     name: "John",
+//     age: 30,
+//     city: "New York"
+// };
+// for (const key in object) {
+    
+    
+//     console.log(key, object[key]);
+    
+    
+// }
+// for (const char of ("Hello World")) {
+//     console.log(char);
+// }
+// while (i< 10) {
+    //     console.log(i);
+    //     i++;
+    // }
+    let i = 0;
+do {
+    console.log(i);
+    i++;
+}while (i < 5);
